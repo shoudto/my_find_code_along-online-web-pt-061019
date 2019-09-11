43eebf865bf141c6d4b055e5d3b0c24f89f97ca0
Oscar Shoudt <shoudto@gmail.com>
2019-09-11 16:01:54 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,11 +1,12 @@
+
 require 'pry'
 
 def my_find(collection)
-i = 0
-while i < collection.length
+i = 0 
+while i < collection.length 
   if yield(collection[i])
     return collection[i]
-  end
-  i += 1
-end
+  end 
+  i += 1 
+end 
 end
